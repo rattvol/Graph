9e8af847aac6144225bb19a56cf07fe8dc852b30
Ratt <ratt_vol@mail.ru>
2019-12-08 21:37:13 +0300

    Изменена структура, убран app.config, убрана проверка наличия файла

--- Graph/Program.cs
+++ Graph/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 
 namespace Graph
@@ -12,81 +8,79 @@ namespace Graph
     {
         static void Main(string[] args)
         {
-            string inputPath = Properties.Settings.Default.inputPath;
-            string outputPath = Properties.Settings.Default.outputPath;
-            FileInfo inputFile = new FileInfo(inputPath);
-            if (inputFile.Exists)
+            int numOfNodes, numOfRibs;
+            int[,] arrOfRibs;
+            string result = "";
+            using (StreamReader streamReader = new StreamReader("input.txt"))
             {
-                try
+                string line = ClearLine(streamReader.ReadLine());
+                string[] objOfLine = line.Split(' ');
+                numOfNodes = Convert.ToInt32(objOfLine[0]);//количество вершин
+                numOfRibs = Convert.ToInt32(objOfLine[1]);//количество ребер
+                //загрузка массива ребер
+                arrOfRibs = new int[numOfRibs, 2];
+                for (int i = 0; i < numOfRibs; i++)
                 {
-                    //подключение файла
-                    FileStream fs = inputFile.OpenRead();
-                    StreamReader streamReader = new StreamReader(fs);
-                    string line = streamReader.ReadLine();
-                    line = Regex.Replace(line, "[ ]+", " ").Trim();
-                    string[] objOfLine = line.Split(' ');
-                    int numOfNodes = Convert.ToInt32(objOfLine[0]);//количество вершин
-                    int numOfRibs = Convert.ToInt32(objOfLine[1]);//количество ребер
-                    //загрузка массива ребер
-                    int[,] arrOfRibs = new int[numOfRibs, 2];
-                    for (int i = 0; i < numOfRibs; i++)
-                    {
-                        do{
-                        line = streamReader.ReadLine();
-                            line = Regex.Replace(line, "[ ]+", " ").Trim();
-                        }
-                        while (line == "") ;//пропуск пустых строк
-                        objOfLine = line.Split(' ');
-                        arrOfRibs[i, 0] = Convert.ToInt32(objOfLine[0]);
-                        arrOfRibs[i, 1] = Convert.ToInt32(objOfLine[1]);
+                    do{
+                    line = ClearLine(streamReader.ReadLine());
                     }
-                    streamReader.Close();
-                    //первичное заполнение массива степеней вершин
-                    int[] arrOfNodes = new int[numOfNodes];
-                    for (int i = 0; i < numOfNodes; i++)
-                    {
-                        arrOfNodes[i] = 0;//по умолчанию степень 0
-                        for (int n = 0; n < numOfRibs; n++)
-                        {
-                            if (arrOfRibs[n, 0] == i + 1 || arrOfRibs[n, 1] == i + 1) arrOfNodes[i]++;//если ребро содержит номер вершины добавляем в массив
-                        }
-
-                    }
-                    //Сортировка массива по убыванию
-                    for (int i = 0; i < numOfNodes-1; i++)
-                    {
-                        for (int n = 0; n < (numOfNodes - (i+1)); n++)
-                        {
-                            if (arrOfNodes[n] < arrOfNodes[n+1])
-                            {
-                                int temp = arrOfNodes[n + 1];
-                                arrOfNodes[n + 1] = arrOfNodes[n];
-                                arrOfNodes[n] = temp;
-                            }
-                        }
-                    }
-
-                    //вывод в файл
-                    StreamWriter streamWriter = new StreamWriter(outputPath, false);
-                    for (int i = 0; i < numOfNodes; i++)
-                    {
-                        streamWriter.Write(arrOfNodes[i].ToString() +" ");
-                    }
-                    streamWriter.WriteLine();
-                    Console.WriteLine("Результат записан в файл:" + outputPath);
-                    streamWriter.Close();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
+                    while (line == "") ;//чтение с пропуском пустых строк
+                    objOfLine = line.Split(' ');
+                    arrOfRibs[i, 0] = Convert.ToInt32(objOfLine[0]);
+                    arrOfRibs[i, 1] = Convert.ToInt32(objOfLine[1]);
                 }
 
             }
-            else
+            //первичное заполнение массива степеней вершин
+            int[] arrOfNodes = new int[numOfNodes];
+            MassFiling(ref arrOfNodes, arrOfRibs, numOfNodes, numOfRibs);
+            //Сортировка массива по убыванию
+            Sort(ref arrOfNodes, numOfNodes);
+            //запись результата в строку
+           result = ToLine(arrOfNodes, numOfNodes);
+            //вывод в файл
+            using (StreamWriter streamWriter = new StreamWriter("output.txt", false))
+                streamWriter.WriteLine(result);
+        }
+        public static string ClearLine (string line)//пропуск пустых строк и двойных пробелов
+        {
+            return Regex.Replace(line, "[ ]+", " ").Trim();
+        }
+        public static void MassFiling(ref int[] array, int[,] readArray, int numOfNodes, int numOfRibs)
+        {
+            for (int i = 0; i < numOfNodes; i++)
             {
-                Console.WriteLine("Файл "+inputPath+" не найден");
+                array[i] = 0;//по умолчанию степень 0
+                for (int n = 0; n < numOfRibs; n++)
+                {
+                    if (readArray[n, 0] == i + 1 || readArray[n, 1] == i + 1) array[i]++;//если ребро содержит номер вершины добавляем в массив
+                }
+
             }
-            Console.ReadKey();
+        }
+        public static void Sort (ref int[] array, int arrLength)
+        {
+            for (int i = 0; i< arrLength - 1; i++)
+            {
+                for (int n = 0; n<(arrLength - (i+1)); n++)
+                {
+                    if (array[n] < array[n + 1])
+                    {
+                        int temp = array[n + 1];
+                        array[n + 1] = array[n];
+                        array[n] = temp;
+                    }
+}
+            }
+        }
+        public static string ToLine(int[] array, int arrLength)
+        {
+            string output = "";
+            for (int i = 0; i < arrLength; i++)
+            {
+                output += array[i].ToString() + " ";
+            }
+            return output;
         }
     }
 }
